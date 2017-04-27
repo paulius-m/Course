be507292ae8665f158a76cc16c4f391109ecab83
Paulius Morkunas <paulius.morkunas@adform.com>
2017-04-27 19:27:46 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var dbconnection = new SqlConnection("SERVER=localhost;DATABASE=mysql;user=root;password=mpg123"))
+            {
+                dbconnection.Open();
+            }
+        }
+    }
+}
