6c174d54391b90a9edf2ee4ddc159550f2f47c1f
Paulius Morkunas <paulius.morkunas@adform.com>
2017-05-12 19:45:12 +0300

    Pifas50

--- WebApplication/Controllers/ValuesController.cs
+++ WebApplication/Controllers/ValuesController.cs
@@ -9,36 +9,41 @@ namespace WebApplication.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        static List<string> values = new List<string>();
+        
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return values;
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return values[id];
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody]string value)
         {
+            values.Add(value);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
+            
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            values
         }
     }
 }
